3985b4da4dfab86a9c05594266d56f5545888426
Matt Atkinson <matt.atkinson@gmail.com>
2019-05-17 23:39:28 -0700

    Updating application deployment monitor script.

---
+++ ApplicationDeploymentMonitor/ApplicationDeploymentMonitor.ps1
@@ -0,0 +1,131 @@
+<#
+.SYNOPSIS
+    Use with an ConfigMgr status filter rule to automatically receive alerts for deployments to collections larger than a specied threshold.
+.DESCRIPTION
+    Full installation procedures can be found on my blog: http://blog.configmatt.com/2017/05/monitoring-potentially-dangerous.html
+.EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+.INPUTS
+    Inputs (if any)
+.OUTPUTS
+    Output (if any)
+.NOTES
+    Author: Matt Atkinson @ConfigMatt www.configmatt.com
+.PARAMETER AssignmentID
+AssigmentID of the deployment. Should be passed direcly from the status filter rule.
+.PARAMETER Creator
+AD account of the person that created the deployment. Will be passed from the status filter rule.
+#>
+param (
+$assignmentID,
+$Creator
+)
+#Import the ConfigMgr Powershell Module
+Import-Module (Join-Path $(Split-Path $env:SMS_ADMIN_UI_PATH) ConfigurationManager.psd1)
+
+## Declare variables
+#Comma separated list of email addresses to send warning to, update this for your organization.
+$EmailAddresses = $creatoremail, "user@domain.com", "user2@domain.com"
+
+#Email server, update this for your organization.
+$EmailServer = "smtp.emailserver.com"
+
+#Email server port
+$EmailPort = "25"
+
+#Email address to use as the sender
+$sender = "DeploymentWarning@domain.com"
+
+#SCCM Site Code for your SCCM site
+$CMSiteCode = "ABC:"
+
+#SCCM Site Server
+$SiteServer = "ConfigMgr.domain.com"
+
+#Number of computers to be the warning threshold. If the deployment goes to more than this number of computers/users, warning will be sent
+$WarningThreshold = 500
+
+#Parse the value supplied as the creator parameter to determine the name of the user and their email.
+$creatorEmail = $Creator.Split("\")
+$creatorName = (get-aduser -Identity $creatorEmail[1] -Server $creatorEmail[0] -Properties name | Select-Object -ExpandProperty name)
+$creatorEmail = (get-aduser -Identity $creatorEmail[1] -Server $creatorEmail[0] -Properties mail | Select-Object -ExpandProperty mail)
+
+#Switch to the CMSite PSDrive
+Set-location $CMSiteCode
+
+#Modify the maximum number of ConfigMgr query results to return, in case you have a very large number of deployments.
+Set-CMQueryResultMaximum 5000
+
+$AssignmentUniqueID = Get-CMDeployment | Where-Object {$_.AssignmentID -eq $assignmentID} | Select-Object -ExpandProperty DeploymentID
+$Deployment = Get-CMDeployment -DeploymentId $AssignmentUniqueID
+
+#Get the application name
+$Application = $Deployment.SoftwareName
+
+#Get the config type (required or available)
+$DesiredConfigType = $Deployment.DesiredConfigType
+
+#Get the deployment start time
+$AvailableTime = $Deployment.DeploymentTime
+
+#Convert UTC to local time
+$AvailableTime = $AvailableTime.ToLocalTime()
+$AvailableTime = $AvailableTime.ToString()
+
+#Get the deadline time
+$DeadlineTime = $Deployment.EnforcementDeadline
+
+#Convert UTC to local time
+$DeadlineTime = $DeadlineTime.ToLocalTime()
+$DeadlineTime = $DeadlineTime.ToString()
+
+#Get The comment
+$Comment = (Get-WmiObject -ComputerName $SiteServer -Namespace Root\SMS\Site_$CMSiteCode -class SMS_ApplicationAssignment -Filter "AssignmentID = '$($deployment.assignmentid)'").Assignmentdescription
+
+#Switch for the desired config (Install or Uninstall)
+Switch ($DesiredConfigType)
+    {
+     1{$DesiredConfigType = "Installed"}
+     2{$DesiredConfigType = "Uninstalled"}
+    }
+
+#Switch for the deployment intent (Available or Required)
+$DeploymentIntent = $Deployment.DeploymentIntent
+
+Switch ($DeploymentIntent)
+    {
+     1{$DeploymentIntent = "Required"}
+     2{$DeploymentIntent = "Available"}
+    }
+#Get the collection that is targeted
+$TargetCollection = $Deployment.CollectionName
+
+#Get the member count of the collection after testing whether it is a user or device collection
+$MemberCount = (Get-CMCollection -name "$TargetCollection").MemberCount
+
+If ((Get-CMCollection -Name "$TargetCollection").Collectiontype -eq "1")
+    {
+        
+        $ClientType = "Users"
+    }
+Else
+    {
+        $ClientType = "Computers"
+    }
+
+If ($MemberCount -ge $WarningThreshold)
+    {
+
+        if ($DeploymentIntent -eq "Required")
+        {
+        Send-MailMessage -SmtpServer $EmailServer -Port $EmailPort -Priority High -From $sender -To $EmailAddresses -Subject "Deployment Warning - Required to $MemberCount $clienttype" -Body  "$Application is being $DesiredConfigType on $MemberCount assets in the collection $Targetcollection by $creatorname.`n`nThe deployment type is $DeploymentIntent and will become available at $AvailableTime Pacific Time and has an install deadline of $DeadlineTime Pacific Time.`n The Assignment ID is $AssignmentUniqueID. The comments on the deployment are: `n $Comment `n `n "
+        }
+
+        elseif ($DeploymentIntent -eq "Available")
+        {
+        Send-MailMessage -SmtpServer $EmailServer -Port $EmailPort -Priority Low -From $sender -To $EmailAddresses -Subject "Deployment Warning - Available to $MemberCount $clienttype" -Body  "$Application is being $DesiredConfigType on $MemberCount assets in the collection $Targetcollection by $creatorname.`n`nThe deployment type is $DeploymentIntent and will become available at $AvailableTime.`n The Assignment ID is $AssignmentUniqueID. The comments on the deployment are: `n $Comment"
+        }
+
+
+    } 
