ce936479104e028eaa81d35d267278d51dd7433a
Matt Atkinson <matt.atkinson@gmail.com>
2019-05-20 08:56:40 -0700

    Modified comments.

--- PackageDeploymentMonitor/PackageDeploymentMonitor.ps1
+++ PackageDeploymentMonitor/PackageDeploymentMonitor.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    Use with an ConfigMgr status filter rule to automatically receive alerts for deployments to collections larger than a specied threshold.
+    Use with a ConfigMgr status filter rule to automatically receive alerts for deployments to collections larger than a specied threshold.
 .DESCRIPTION
     Full installation procedures can be found on my blog: http://blog.configmatt.com/2017/05/monitoring-potentially-dangerous.html
 .EXAMPLE
@@ -17,7 +17,7 @@ AssigmentID of the deployment. Should be passed direcly from the status filter r
 .PARAMETER Creator
 AD account of the person that created the deployment. Will be passed from the status filter rule.
 
-##TODO: Add teams notification options to public script.
+##TODO: Add MS Teams notification options to public script.
 #>
 
 param (
